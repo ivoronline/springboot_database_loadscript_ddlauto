d295a24128fb902c0faabab0bb694a751844a419
ivoronline <ivoronline@gmail.com>
2024-07-21 11:18:32 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_database_loadscript_ddlauto/SpringbootDatabaseLoadscriptDdlautoApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_database_loadscript_ddlauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatabaseLoadscriptDdlautoApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootDatabaseLoadscriptDdlautoApplication.class, args);
+  }
+  
+}
---
+++ src/test/java/com/ivoronline/springboot_database_loadscript_ddlauto/SpringbootDatabaseLoadscriptDdlautoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_database_loadscript_ddlauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatabaseLoadscriptDdlautoApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
